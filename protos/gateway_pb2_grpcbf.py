de31651d51f44a3375a3a62409f4ebef5a29b85d
jossemii <josemi.bnf@gmail.com>
2022-10-31 19:00:32 +0100

    asd

--- protos/gateway_pb2_grpcbf.py
+++ protos/gateway_pb2_grpcbf.py
@@ -1,11 +1,11 @@
-from protos import buffer_pb2, gateway_pb2
+from protos import buffer_pb2, gateway_pb2, service_capnp
 
 StartService_input = {
     5 : gateway_pb2.Client,
     6 : gateway_pb2.RecursionGuard,
 
     1 : gateway_pb2.celaut__pb2.Any.Metadata.HashTag.Hash,
-    2 : gateway_pb2.ServiceWithMeta,
+    2 : service_capnp.ServiceWithMeta,
     3 : gateway_pb2.HashWithConfig,
     4 : gateway_pb2.ServiceWithConfig
 }
@@ -105,14 +105,14 @@ GetServiceEstimatedCost_input = {
     6 : gateway_pb2.RecursionGuard,
 
     1 : gateway_pb2.celaut__pb2.Any.Metadata.HashTag.Hash,
-    2 : gateway_pb2.ServiceWithMeta,
+    2 : service_capnp.ServiceWithMeta,
     3 : gateway_pb2.HashWithConfig,
     4 : gateway_pb2.ServiceWithConfig,
 }
 
 GetServiceTar_input = {
     1 : gateway_pb2.celaut__pb2.Any.Metadata.HashTag.Hash,
-    2 : gateway_pb2.ServiceWithMeta,
+    2 : service_capnp.ServiceWithMeta,
 }
 
 Compile_output_partitions_v1 = [
