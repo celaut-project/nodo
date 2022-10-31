b1dcd6583015862381d1cd36b2a1633055d4ab59
jossemii <josemi.bnf@gmail.com>
2022-10-31 18:59:24 +0100

    asd

--- protos/gateway_pb2_grpc.py
+++ protos/gateway_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import buffer_pb2 as buffer__pb2
+import protos.buffer_pb2 as buffer__pb2
 
 
 class GatewayStub(object):
