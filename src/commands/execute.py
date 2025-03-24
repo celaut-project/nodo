7ed7dd9f46d336b29aeb5451af77111943bd1846
0xf965 <thisemaildoesnotexists@anon.erg>
2025-03-24 09:13:22 +0100

    more logs for debug

--- src/commands/execute.py
+++ src/commands/execute.py
@@ -22,8 +22,9 @@ SHA3_256 = SHA3_256_ID.hex()
 def generator(_hash: str, mem_limit: int = 50 * pow(10, 6), initial_gas_amount: int = DEFAULT_INTIAL_GAS_AMOUNT) -> Generator[Any, None, None]:
     print("Get clients")
     clients = get_dev_clients(gas_amount=initial_gas_amount)
-    client_id = next(clients)
-    if not client_id:
+    try:
+        client_id = next(clients)
+    except Exception:
         print("There is no dev client available")
         exit
     print(f"Client obtained {str(client_id)}")
