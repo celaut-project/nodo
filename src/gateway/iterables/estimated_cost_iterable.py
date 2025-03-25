bb72a528685ab8051baa9df05da52d84e6f4c6df
0xf965 <thisemaildoesnotexists@anon.erg>
2025-03-25 09:30:17 +0100

    recursion guard on estimated cost iterable

--- src/gateway/iterables/estimated_cost_iterable.py
+++ src/gateway/iterables/estimated_cost_iterable.py
@@ -3,6 +3,7 @@ from typing import Optional, Generator
 from bee_rpc import client as bee, buffer_pb2
 
 from protos import gateway_pb2
+from src.utils.tools.recursion_guard import RecursionGuard
 from src.virtualizers.docker import build
 from src.gateway.iterables.abstract_service_iterable import AbstractServiceIterable, BreakIteration
 from src.manager.manager import default_initial_cost
@@ -13,7 +14,9 @@ from src.utils.utils import from_gas_amount, get_only_the_ip_from_context
 
 class GetServiceEstimatedCostIterable(AbstractServiceIterable):
     
-    # Actually there is not need to use recursion guard, there are any recursive call pattern here. https://github.com/celaut-project/nodo/issues/70
+    # https://github.com/celaut-project/nodo/issues/70
+    
+    # Although this call does not perform recursion on the peers (it only returns the local estimated cost and not that of the peers), the estimated cost could be requested by the peer that was just asked to execute the service.
 
     cost: Optional[int] = None
 
@@ -22,27 +25,31 @@ class GetServiceEstimatedCostIterable(AbstractServiceIterable):
         return super().start()
 
     def generate(self) -> Generator[buffer_pb2.Buffer, None, None]:
-        try:
-            initial_gas_amount: int = from_gas_amount(self.configuration.initial_gas_amount) \
-                if self.configuration.HasField('initial_gas_amount') \
-                else default_initial_cost(
-                    father_id=self.client_id if self.client_id
-                        else get_only_the_ip_from_context(context_peer=self.context.peer())
-                    )
+        with RecursionGuard(
+                token=self.recursion_guard_token,
+                generate=True
+        ) as recursion_guard_token:
+            try:
+                initial_gas_amount: int = from_gas_amount(self.configuration.initial_gas_amount) \
+                    if self.configuration.HasField('initial_gas_amount') \
+                    else default_initial_cost(
+                        father_id=self.client_id if self.client_id
+                            else get_only_the_ip_from_context(context_peer=self.context.peer())
+                        )
 
-            yield from bee.serialize_to_buffer(
-                message_iterator=generate_estimated_cost(
-                    metadata=self.metadata,
-                    initial_gas_amount=initial_gas_amount,
-                    config=self.configuration
-                ),
-                indices=gateway_pb2.EstimatedCost
-            )
-        except build.UnsupportedArchitectureException as e:
-            raise e
-        finally:
-            yield buffer_pb2.Buffer(signal=True)
-            raise BreakIteration
+                yield from bee.serialize_to_buffer(
+                    message_iterator=generate_estimated_cost(
+                        metadata=self.metadata,
+                        initial_gas_amount=initial_gas_amount,
+                        config=self.configuration
+                    ),
+                    indices=gateway_pb2.EstimatedCost
+                )
+            except build.UnsupportedArchitectureException as e:
+                raise e
+            finally:
+                yield buffer_pb2.Buffer(signal=True)
+                raise BreakIteration
 
     def final(self):
         log('End request for the cost of a service.')
