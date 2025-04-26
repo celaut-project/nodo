b3fade20081bbf843589ac4076c1425475d2eb5e
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-04-26 22:13:35 +0200

    improved logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,9 +5,17 @@ from src.utils.env import EnvManager
 env_manager = EnvManager()
 STORAGE, USE_PRINT = env_manager.get_env("STORAGE"), env_manager.get_env("USE_PRINT")
 
-if not os.path.exists(STORAGE): os.makedirs(STORAGE)
+if not os.path.exists(STORAGE):
+    os.makedirs(STORAGE)
 
-logging.basicConfig(filename=f'{STORAGE}/app.log', level=logging.INFO,
-                    format='%(asctime)s %(levelname)-8s %(message)s')
-LOGGER = (lambda message: print(message + '\n')) if USE_PRINT \
-    else (lambda message: logging.getLogger(__name__).info(message + '\n'))
+logging.basicConfig(
+    filename=f'{STORAGE}/app.log',
+    level=logging.INFO,
+    format='%(message)s'
+)
+
+LOGGER = (
+    lambda message: print(message + '\n')
+) if USE_PRINT else (
+    lambda message: logging.getLogger(__name__).info(message + '\n')
+)
