768306b57b2c03781de668c7a8adbfeaa3ccd07c
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-04-27 18:17:31 +0200

    gateway port

--- src/serve.py
+++ src/serve.py
@@ -7,6 +7,10 @@ from protos import gateway_pb2, gateway_pb2_grpc
 from src.gateway.gateway import Gateway
 from src.tunneling_system.tunnels import TunnelSystem
 from src.manager.maintain_thread import manager_thread
+from src.utils.env import EnvManager
+
+env_manager = EnvManager()
+GATEWAY_PORT = env_manager.get_env("GATEWAY_PORT")
 
 def serve():
 
