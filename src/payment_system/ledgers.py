9f747c624cfd9b656c9723677f5fe97b67da7f72
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-04-30 22:14:56 +0200

    using gas price on api, instead of contract ledger directly.

--- src/payment_system/ledgers.py
+++ src/payment_system/ledgers.py
@@ -2,10 +2,22 @@ from typing import Generator
 from protos import celaut_pb2 as celaut
 from src.database.access_functions.ledgers import get_ledger_and_contract_addr_from_contract
 from src.payment_system.contracts.ergo.interface import CONTRACT_HASH, CONTRACT
+from src.utils.env import EnvManager
+from src.utils.utils import to_gas_amount
 
-def local_payment_methods() -> Generator[celaut.ContractLedger, None, None]:
+ERGO_GAS_COST = EnvManager().get_env("ERGO_GAS_COST")
+
+def local_payment_methods() -> Generator[celaut.GasPrice, None, None]:
+
     for address, ledger in get_ledger_and_contract_addr_from_contract(CONTRACT_HASH):
+
         contract_ledger = celaut.ContractLedger()
         contract_ledger.contract = CONTRACT
         contract_ledger.contract_addr, contract_ledger.ledger = address, ledger
-        yield contract_ledger
+
+        gas_price = celaut.GasPrice(
+            contract_ledger=contract_ledger,
+            gas_amount=to_gas_amount(ERGO_GAS_COST)
+        )
+
+        yield gas_price
