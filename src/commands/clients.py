9078b499e68e4b2cd790fd2bf1dbf8a8c4441d62
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-05-06 17:17:43 +0200

    use int instead of float.

--- src/commands/clients.py
+++ src/commands/clients.py
@@ -47,9 +47,9 @@ def list_clients():
 
             # Section: Gas & Usage
             print("[Gas & Usage]")
-            print(f"  Gas: {ssformat(float(gas))}")
+            print(f"  Gas: {ssformat(int(gas))}")
             print((gas))
-            print(float(gas))
+            print(int(gas))
             print(f"  Last Usage: {last_usage if last_usage is not None else 'None'}")
             print()
 
