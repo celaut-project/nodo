559654198f791dc201ed8a1a4710d3030473c685
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-05-14 17:43:14 +0200

    deleted prints used for debug.

--- src/gateway/iterables/abstract_input_service_iterable.py
+++ src/gateway/iterables/abstract_input_service_iterable.py
@@ -59,7 +59,7 @@ class AbstractInputServiceIterable:
             indices=StartService_input_indices,
             partitions_message_mode=StartService_input_message_mode
         )
-        
+
         self.context = context
 
         self.configuration: Optional[gateway_pb2.Configuration] = None
@@ -87,22 +87,12 @@ class AbstractInputServiceIterable:
                 self.configuration = r
 
             case celaut.Metadata.HashTag.Hash:
-                
-                print(f"Hash recibida: {r.type.hex()}  :   {r.value.hex()}")
-
                 self.hashes.add(Hash(r))
-                
-                print(f"Actual hashes set: {[h.type.hex()+'  '+h.value.hex() for h in self.hashes]}\n\n")
-                
                 if not self.service_hash:
                     self.service_hash, self.service_saved = find_service_hash(_hash=r)
 
             case celaut.Metadata:
                 self.metadata = r
-
-                print("Metadatos recibidos.")
-                for hash in list(self.metadata.hashtag.hash):
-                        print(f"-  {hash.type.hex()}: {hash.value.hex()}")
 
                 for _hash in self.metadata.hashtag.hash:  # TODO nos podríamos ahorrar esta iteración
                     if not self.service_hash:
@@ -110,12 +100,9 @@ class AbstractInputServiceIterable:
                     # TODO se podría realizar junto con la iteració siguiente:
 
                 # Combine the hash list with the metadata hashes.
-                print(f"Actual hashes set: {[h.type.hex()+'  '+h.value.hex() for h in self.hashes]}. Realiza union....")
                 self.hashes: Set[Hash] = self.hashes.union({
                     Hash(_e) for _e in self.metadata.hashtag.hash
                 })
-
-                print(f"Actual hashes set despues de union: {[h.type.hex()+'  '+h.value.hex() for h in self.hashes]}\n\n")
 
                 self.metadata.hashtag.ClearField("hash")
                 self.metadata.hashtag.hash.extend([_e.proto() for _e in self.hashes])
