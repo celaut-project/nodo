e40cdb7acf770e7647cc9ca47669e02f1b4b4779
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-05-14 17:16:29 +0200

    fixe.d

--- src/gateway/iterables/abstract_input_service_iterable.py
+++ src/gateway/iterables/abstract_input_service_iterable.py
@@ -90,7 +90,7 @@ class AbstractInputServiceIterable:
 
                 self.hashes.add(Hash(r))
                 
-                print(f"Actual hashes set: {[h.type.hex()+"  "+h.value.hex() for h in self.hashes]}\n\n")
+                print(f"Actual hashes set: {[h.type.hex()+'  '+h.value.hex() for h in self.hashes]}\n\n")
                 
                 if not self.service_hash:
                     self.service_hash, self.service_saved = find_service_hash(_hash=r)
@@ -108,12 +108,12 @@ class AbstractInputServiceIterable:
                     # TODO se podría realizar junto con la iteració siguiente:
 
                 # Combine the hash list with the metadata hashes.
-                print(f"Actual hashes set: {[h.type.hex()+"  "+h.value.hex() for h in self.hashes]}. Realiza union....")
+                print(f"Actual hashes set: {[h.type.hex()+'  '+h.value.hex() for h in self.hashes]}. Realiza union....")
                 self.hashes: Set[Hash] = self.hashes.union({
                     Hash(_e) for _e in self.metadata.hashtag.hash
                 })
 
-                print(f"Actual hashes set despues de union: {[h.type.hex()+"  "+h.value.hex() for h in self.hashes]}\n\n")
+                print(f"Actual hashes set despues de union: {[h.type.hex()+'  '+h.value.hex() for h in self.hashes]}\n\n")
 
                 self.metadata.hashtag.ClearField("hash")
                 self.metadata.hashtag.hash.extend([_e.proto() for _e in self.hashes])
