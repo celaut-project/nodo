e85c9bed207ca2a4f9cc3132c20ceb8f76bca4e1
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-06-30 09:17:10 +0200

    feat: implement reputation token contract with admin and public top-up paths

--- src/reputation_system/envs.py
+++ src/reputation_system/envs.py
@@ -3,17 +3,4 @@ from src.utils.env import EnvManager
 
 env_manager = EnvManager()
 LEDGER = "ergo" # or "ergo-testnet" for Ergo testnet.
-CONTRACT = """{
-    SELF.R7[SigmaProp].get &&
-    sigmaProp(SELF.tokens.size == 1) &&
-    sigmaProp(OUTPUTS.forall { (x: Box) =>
-    !(x.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == SELF.tokens(0)._1 }) ||
-    (
-        x.R7[SigmaProp].get == SELF.R7[SigmaProp].get &&
-        x.tokens.size == 1 &&
-        x.propositionBytes == SELF.propositionBytes &&
-        (x.R8[Boolean].get == false || x.R8[Boolean].get == true)
-    )
-    })
-}"""
-
+CONTRACT = open("contracts/reputation_system.es", "r").read()
