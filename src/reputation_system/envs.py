19d0dfb034573bd17ac1dfee749f1fe76af39012
0xf965 <thisemaildoesnotexists@anon.erg>
2025-06-30 09:19:16 +0200

    refactor: remove unused EnvManager import and instance from envs.py

--- src/reputation_system/envs.py
+++ src/reputation_system/envs.py
@@ -1,6 +1,3 @@
-from src.utils.env import EnvManager
 
-
-env_manager = EnvManager()
 LEDGER = "ergo" # or "ergo-testnet" for Ergo testnet.
 CONTRACT = open("contracts/reputation_system.es", "r").read()
