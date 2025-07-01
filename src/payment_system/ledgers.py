d86b372e2b6e823cf512a315b63f3ba37d48e4d9
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-07-01 17:25:28 +0200

    fix: update logging in local_payment_methods for better visibility of ledger usage

--- src/payment_system/ledgers.py
+++ src/payment_system/ledgers.py
@@ -4,12 +4,15 @@ from src.database.access_functions.ledgers import get_ledger_and_contract_addr_f
 from src.payment_system.contracts.ergo.interface import CONTRACT_HASH, CONTRACT
 from src.utils.env import EnvManager
 from src.utils.utils import to_gas_amount
+from src.utils.logger import LOGGER
 
 GAS_PER_ERG = int(EnvManager().get_env("GAS_PER_ERG"))
 
 def local_payment_methods() -> Generator[celaut.GasPrice, None, None]:
     for address, ledger in get_ledger_and_contract_addr_from_contract(CONTRACT_HASH): 
-            
+        
+        LOGGER(f"Using ledger {ledger} with address {address} for contract {CONTRACT_HASH}")
+
         contract_ledger = celaut.ContractLedger()
         contract_ledger.contract = CONTRACT
         contract_ledger.contract_addr, contract_ledger.ledger = address, ledger
