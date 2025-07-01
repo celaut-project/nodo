87d372cd3e6212191d213f14a07f9e01c910054c
0xf965 <thisemaildoesnotexists@anon.erg>
2025-07-01 17:44:19 +0200

    fix: add check for empty local payment contracts before extending instance.api.payment_contracts

--- src/gateway/utils.py
+++ src/gateway/utils.py
@@ -49,7 +49,8 @@ def generate_node_peer_info(network: str) -> celaut_pb2.Peer:
 
     payment_contracts = [e for e in local_payment_methods()]
     log.LOGGER(f'Using {len(payment_contracts)} local payment methods')
-    instance.api.payment_contracts.extend(payment_contracts)
+    if payment_contracts:
+        instance.api.payment_contracts.extend(payment_contracts)
 
     return celaut_pb2.Peer(
         reputation_proofs=list(local_proofs()),
