10f0899fe6fdcc62658671db12dbe1417daffc55
0xf965 <0xf965@users.noreply.github.com>
2026-03-18 12:12:23 +0100

    remove virtual machine

--- src/commands/remove.py
+++ src/commands/remove.py
@@ -1,18 +1,14 @@
 import os
 import shutil
-import subprocess
 from src.commands.__by_tag import get_id
-from src.utils.config import ConfigManager, DOCKER_COMMAND, DOCKER_ENV
-from src.utils.logger import LOGGER as l
+from src.utils.config import ConfigManager
+from src.virtualizers.interface import remove as remove_vm
 
 env_manager = ConfigManager()
 
 METADATA_REGISTRY = env_manager.get("METADATA_REGISTRY")
 REGISTRY = env_manager.get("REGISTRY")
 DEFAULT_INITIAL_GAS_AMOUNT = env_manager.get("DEFAULT_INITIAL_GAS_AMOUNT")
-
-
-# TODO This command must be generalized into virtualizers/interface.  But first DB must contain the virtualizer used by the vmachine.
 
 def remove(service: str):
     service = get_id(service)
@@ -22,15 +18,7 @@ def remove(service: str):
         print("This script requires superuser privileges. Please run with sudo.")
         return
 
-    try:
-        subprocess.run(
-            DOCKER_COMMAND + ["rmi", f"{service}.docker", "--force"],
-            env=DOCKER_ENV,
-            check=True
-        )
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing docker rmi: {e}")
-        raise
+    remove_vm(service)
 
     registry_path = os.path.join(REGISTRY, service)
     if os.path.isfile(registry_path):
--- src/virtualizers/interface.py
+++ src/virtualizers/interface.py
@@ -1,6 +1,7 @@
 from src.virtualizers.docker.build import is_service_built as docker_is_service_built
 from src.virtualizers.docker.build import build as docker_build
 from src.virtualizers.docker.execute import execute as docker_execute
+from src.vitualizers.docker.remove import remove as remove_docker
 from src.virtualizers.docker.hotplug import hotplug as docker_hotplug
 from src.virtualizers.docker.kill import kill as docker_kill
 from src.virtualizers.docker.firewall import remove_rule as docker_remove_rule
@@ -85,6 +86,10 @@ def execute(
         initial_system_resources=initial_system_resources,
         father_id=father_id,
     )
+
+def remove(vmachine_id: str) -> bool:
+    """Remove a service."""
+    return remove_docker(vmachine_id=vmachine_id)
 
 def remove_firewall_rule(
         vmachine_id: str,
